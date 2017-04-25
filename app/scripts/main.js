95e975a27278c04352ebc255059485ae55f17aa5
Stephanie Cruz <cruzsv@knights.ucf.edu>
2017-04-24 21:23:47 -0400

    Added animated and descriptions

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   $('#fullpage').fullpage({
 
   anchors: ['page1', 'page2', 'page3', 'page4','page5'],
-  sectionsColor: ['pink', 'orange', '#C0C0C0', '#ADD8E6'],
+  sectionsColor: ['#fce4ec', 'orange', '#C0C0C0', '#ADD8E6'],
   dragAndMove: true,
 
 }); //end fullpage
