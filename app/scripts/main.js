2657521a36e0525268c238779aa873806a22c2b0
Stephanie Cruz <cruzsv@knights.ucf.edu>
2017-05-01 09:50:05 -0400

    Final

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,9 +2,21 @@ $(document).ready(function() {
   $('#fullpage').fullpage({
 
   anchors: ['page1', 'page2', 'page3', 'page4','page5'],
-  sectionsColor: ['#fce4ec', 'orange', '#C0C0C0', '#ADD8E6'],
-  dragAndMove: true,
+  sectionsColor: ['#fce4ec', '#efebe9', '#C0C0C0', '#ADD8E6'],
+
+  //Navigation
+    menu: '#menu',
+    lockAnchors: false,
+    anchors:['firstPage', 'secondPage'],
+    navigation: false,
+    navigationPosition: 'right',
+    navigationTooltips: ['firstSlide', 'secondSlide'],
+    showActiveTooltip: false,
+    slidesNavigation: false,
+    slidesNavPosition: 'bottom',
 
 }); //end fullpage
+
+   $('.tooltipped').tooltip({delay: 50});
 
 }); //end document.ready(function())
